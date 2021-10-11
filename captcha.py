4c1cefa22e8595fdce984da5b9eed0e1a8cfb007
stierlpz <34404904+stierlpz@users.noreply.github.com>
2021-10-11 15:15:31 +0200

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -7,15 +7,16 @@ pip install captcha
 from captcha.image import ImageCaptcha
 
 
-image = ImageCaptcha(width = 280, height = 90)
+image = ImageCaptcha(width = 1000, height = 200)   #<<Größe des Captcha in Pixel width=Breite  heigh=Höhe
 
 
-text=input("Text:  ")
+text=input("Text:  ") #<<Eingabe der Zeichen die im Captca erscheinen sollen
 captcha_text = text 
 
 
 data = image.generate(captcha_text)
 
 
-image.write(captcha_text, 'CAPTCHA.jpg')
-image.write(captcha_text, 'CAPTCHA.png')
+data = image.generate(captcha_text) #<<Umwandlung in Captcha
+image.write(captcha_text, 'captcha.jpg') #<<Ausgabe als jpg
+image.write(captcha_text, 'captcha.png') #<<Ausgabe als png
